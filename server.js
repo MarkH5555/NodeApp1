a6a7a846bc8bb0212106e37c80ed773c46eca655
Mark Harrison <markharrison@Marks-MacBook-Pro.local>
2018-08-07 08:36:23 +0100

    Simple React client added

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-const express = require("express");
+const express 	= require("express");
+const path 		= require('path');
 
 const app = express();
-
 const port = 8000;
 
-const path = require('path');
 
-// Adding this next line removes the need to add get functions to serve the html and css
-app.use(express.static('public'));
+const users = [
+	{id: '1', cName: 'Adam', sName: 'Smith'},
+	{id: '2',cName: 'Ben', sName: 'Jones'},
+	{id: '3',cName: 'Colin', sName: 'Banks'},
+	{id: '4',cName: 'Dave', sName: 'Yerkess'},
+	{id: '5',cName: 'Ed', sName: 'Millar'}
+];
 
 
+app.get('/api/users', (req, res) => {
+	console.log('API data request on: /api/users ');
+
+	res.json(users);
+});
 
 
 
 app.listen(port, () => {
 	console.log("Listening on port " + port);
-
-})
+});
